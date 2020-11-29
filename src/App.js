42b3952daf8f9d990de7c77369b50a994919d926
Eugie Limpin <eugiellimpin@gmail.com>
2020-11-29 12:00:59 +0800

    Set up react-dates lib

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// react-dates
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
+
+import React from 'react';
+import { SingleDatePicker } from 'react-dates';
 
 function App() {
+  const [date, setDate] = React.useState(null);
+  const [open, setOpen] = React.useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello there!</h1>
+      <SingleDatePicker
+        date={date}
+        onDateChange={date => setDate(date)}
+        focused={open}
+        onFocusChange={({ focused }) => setOpen(focused)}
+        id="delivery-date"
+      />
     </div>
   );
 }
